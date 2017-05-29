7f57305a5ed04c2ffed5cf26cfc9145e9f6db82a
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-05-29 23:41:52 +0100

    added tests of the read / write and wavelengths

--- trm/molly/tests/molly_test.py
+++ trm/molly/tests/molly_test.py
@@ -1,23 +1,22 @@
 import unittest
 import copy
+import tempfile
 
 import numpy as np
 from collections import OrderedDict as Odict
 from trm import molly
 
 class TestMolly(unittest.TestCase):
-    """Provides simple tests of CCD methods and attributes.
-
+    """Provides simple tests of trm.molly classes and methods
     """
 
     def setUp(self):
 
         self.npix = 1500
-        xpix = np.linspace(1,self.npix,self.npix) / npix
-        xpix -= xpix.mean()
-        self.w = 4500. + 1000*xpix + 40.*xpix**2
-        self.f = np.zeros_like(self.w)
-        self.f += 1
+        self.wave1 = 3800.
+        self.wave2 = 5200.
+        self.w = np.linspace(self.wave1,self.wave2,self.npix)
+        self.f = np.ones_like(self.w)
         self.f += 0.6*np.exp(-((self.w-4600.)/30.)**2/2.)
         self.cfrat = 1000.*np.ones_like(self.w)
         counts = self.f*self.cfrat
@@ -31,11 +30,62 @@ class TestMolly(unittest.TestCase):
         self.head['Expose'] = np.float32(120.)
         self.head['Record'] = 12345
 
+        # make a spectrum
+        self.spec = molly.Molly.fromdata(
+            self.w,self.f,self.fe,self.cfrat,self.head
+        )
+
     def test_fromdata(self):
-        spec = molly.Molly.fromdata(
-            self.w,self.f,self.fe,self.cfrat,self.head)
-        self.assertEqual(spec.narc,3,
-                         'incorrect number of arc coefficients')
+        self.assertEqual(
+            self.spec.narc, 2,
+            'incorrect number of arc coefficients'
+        )
+        self.assertEqual(
+            self.spec.npix, self.npix,
+            'incorrect number of pixels'
+        )
+        self.assertAlmostEqual(
+            self.spec.arc[0]+self.spec.arc[1]/self.npix, self.wave1,
+            7, 'wavelength of first pixel wrong'
+        )
+        self.assertAlmostEqual(
+            self.spec.arc[0]+self.spec.arc[1], self.wave2,
+            7, 'wavelength of last pixel wrong'
+        )
+
+    def test_wave(self):
+        w = self.spec.wave
+        self.assertTrue((np.abs(w-self.w) < 1.e-4).all(),
+                        'returned wavelengths are wrong')
+
+    def test_write_read(self):
+        # write to then read from a temporary file
+        with tempfile.TemporaryFile() as fptr:
+            self.spec.write(fptr)
+            fptr.seek(0)
+            spec = molly.Molly.fromfile(fptr)
+
+        # check everything is recovered
+        self.assertEqual(
+            self.spec.npix, spec.npix, 'npix write/read problem'
+        )
+        self.assertEqual(
+            self.spec.narc, spec.narc, 'narc write/read problem'
+        )
+        self.assertEqual(
+            len(self.spec.head), len(spec.head), 'header write/read problem'
+        )
+        for key in self.spec.head.keys():
+            self.assertTrue(key in spec.head,
+                            'header key write/read problem')
+            self.assertTrue(self.spec.head[key] == spec.head[key],
+                            'header value write/read problem')
+        self.assertTrue(
+            (self.spec.wave == spec.wave).all(),
+            'wavelengths write/read problem')
+        self.assertTrue(
+            (np.abs(self.spec.f - spec.f) < 1.e-6).all(),
+            'fluxes write/read problem')
 
 if __name__ == '__main__':
     unittest.main()
